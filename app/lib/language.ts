078c9e4d56bb8c9cd8af18a6d7d53a95eb53fc1e
Marcelo Melogno <marcelomelogno1986@gmail.com>
2025-07-27 13:51:23 -0300

    Implement Language persistence

--- app/lib/language.ts
+++ app/lib/language.ts
@@ -1,13 +1,21 @@
-import {create} from 'zustand';
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-type Language = 'en' | 'es';
+type Language = "en" | "es";
 
 interface LanguageState {
-    language: Language;
-    setLanguage: (language: Language) => void;
+  language: Language;
+  setLanguage: (language: Language) => void;
 }
 
-export const useLanguageStore = create<LanguageState>((set) => ({
-    language: 'en',
-    setLanguage: (language: Language) => set({ language }),
-}));
+export const useLanguageStore = create<LanguageState>()(
+  persist(
+    (set) => ({
+      language: "en",
+      setLanguage: (language: Language) => set({ language }),
+    }),
+    {
+      name: "resumind-language",
+    }
+  )
+);
